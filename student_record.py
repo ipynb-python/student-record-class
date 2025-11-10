093b98110bff89db6c793f7f7ed5e29c3d0e7db4
phil-lewis-exe <p.lewis2@exeter.ac.uk>
2025-11-10 02:30:17 +0000

    auto-commit: 2025m10-023017

--- student_record.py
+++ student_record.py
@@ -11,8 +11,9 @@ class StudentRecord:
         mystr += self.name+"\n"
         mystr += f"Student Number: {self.student_num}\n"
         mystr += f"Degree: {self.degree}\n"
-        mystr += "Modules:"
+        mystr += "Modules:\n"
         for key in self.grade_dict.keys():
+            mystr += ""
 
 
     def enter_grade(self, module_code, grade):
