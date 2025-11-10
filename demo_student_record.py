676f1817ce3b0c102245cab90d5f617530dbd5c4
phil-lewis-exe <p.lewis2@exeter.ac.uk>
2025-11-10 02:26:31 +0000

    auto-commit: 2025m10-022631

--- demo_student_record.py
+++ demo_student_record.py
@@ -4,6 +4,6 @@ student1 = StudentRecord("Emmet Brown", 239, "BSc Physics")
 student1.enter_grade("PHYS205", 55)
 student1.enter_grade("MATH220", 75)
 student1.enter_grade("LANG201", 80)
-student1.enter_grade("COMP", 80)
+student1.enter_grade("COMP101", 30)
 
 print( student1.calculate_grade_average() )
