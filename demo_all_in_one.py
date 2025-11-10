2ac555e047e07ce22dfa21c38bae0c8b519ceeb4
phil-lewis-exe <p.lewis2@exeter.ac.uk>
2025-11-10 02:37:25 +0000

    auto-commit: 2025m10-023725

--- demo_all_in_one.py
+++ demo_all_in_one.py
@@ -1,5 +1,5 @@
 class StudentRecord:
-    def __init__(self, name, student_num, degree, ):
+    def __init__(self, name, student_num, degree, grade_dict=None):
         self.name = name
         self.student_num = student_num
         self.degree = degree
