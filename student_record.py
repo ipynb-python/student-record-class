0e7b9382aef24b5aba6dead11dac4bb60b0136d4
phil-lewis-exe <p.lewis2@exeter.ac.uk>
2025-11-10 02:32:27 +0000

    auto-commit: 2025m10-023227

--- student_record.py
+++ student_record.py
@@ -17,8 +17,7 @@ class StudentRecord:
             grade = self.grade_dict[key]
             mystr += f"  {module}: {grade} \n"
         mystr += f"Average Grade: {self.calculate_grade_average}"
-        
-
+        return mystr
 
     def enter_grade(self, module_code, grade):
         self.grade_dict[module_code] = grade
