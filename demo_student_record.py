46ede59fd64ffd3d0080ae428d85b5e11439b9f4
phil-lewis-exe <p.lewis2@exeter.ac.uk>
2025-11-10 02:21:19 +0000

    auto-commit: 2025m10-022119

--- demo_student_record.py
+++ demo_student_record.py
@@ -2,7 +2,7 @@ from student_record import StudentRecord
 
 student1 = StudentRecord("Emmet Brown", 239, "BSc Physics")
 student1.enter_grade("PHYS205", 55)
-student1.enter_grade("PHYS205", 55)
+student1.enter_grade("MATH220", 55)
 student1.enter_grade("PHYS205", 55)
 
 print(student1.name)
