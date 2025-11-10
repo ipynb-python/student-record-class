587c59a766ebed81b31fd5e60f44f44300c414db
phil-lewis-exe <p.lewis2@exeter.ac.uk>
2025-11-10 02:38:07 +0000

    auto-commit: 2025m10-023807

--- demo_all_in_one.py
+++ demo_all_in_one.py
@@ -33,7 +33,7 @@ class StudentRecord:
         return av_grade
 
 
-student1 = StudentRecord("Emmet Brown", 239, "BSc Physics", )
+student1 = StudentRecord("Emmet Brown", 239, "BSc Physics", {"PHYS"})
 student1.enter_grade("PHYS205", 55)
 student1.enter_grade("MATH220", 75)
 student1.enter_grade("LANG201", 80)
